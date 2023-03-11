e69b6c1c5cd22647525e8355c5dcfb270909afb6
ErnieCed <ernyb100@gmail.com>
2023-03-11 10:47:30 -0500

    actividad7

---
+++ actividad7/database/migrations/2023_03_11_132953_create_cursos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_curso');
+            $table->unsignedBigInteger('id_kit');
+            $table->foreign('id_kit')->references('id')->on('kit_roboticas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cursos');
+    }
+};
